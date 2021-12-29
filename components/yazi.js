c48554f508a2be8a51b9940d544659b312ac33be
bedirhan ergün <bedirhan1029@gmail.com>
2021-12-30 01:16:07 +0300

    init

---
+++ components/yazi.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react'
+
+export class yazi extends PureComponent {
+    render() {
+        return (
+            <div>
+                <h1>hello</h1>
+            </div>
+        )
+    }
+}
+
+export default yazi
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, Button } from 'react-native';
+import { View, StyleSheet, Button, Text } from 'react-native';
 import { signOut } from 'firebase/auth';
 
 import { auth } from '../config';
@@ -11,6 +11,7 @@ export const HomeScreen = () => {
   return (
     <View style={styles.container}>
       <Button title='Sign Out' onPress={handleLogout} />
+      <Text >Step One</Text>
     </View>
   );
 };
